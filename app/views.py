200329717923a2c9cca0c946afb892f888d40291
Kazauwa <i.heart.d.dead@gmail.com>
2016-10-22 17:44:01 +0300

    Added album view with full album description

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ import json
 from flask import render_template, redirect, url_for, session
 from app import app
 from .forms import SearchForm
-from config import HEADERS, DISCOGS_API
+from config import HEADERS, DISCOGS_QUERY, DISCOGS_MASTER
 
 
 @app.route('/')
@@ -24,7 +24,7 @@ def search():
 
 @app.route('/search_results/<query>/', methods=['GET', 'POST'])
 def search_results(query):
-    r = requests.get(DISCOGS_API, params={'q': query, 'type': 'master'}, headers=HEADERS)
+    r = requests.get(DISCOGS_QUERY, params={'q': query, 'type': 'master'}, headers=HEADERS)
     result = {count + 1: {'id': result.get('id'),
                           'title': result.get('title'),
                           'genre': result.get('genre'),
@@ -38,3 +38,16 @@ def search_results(query):
     return render_template('search_results.html',
                             query=query,
                             results=result.values())
+
+
+@app.route('/album/id<master_id>')
+def album(master_id):
+    r = requests.get(DISCOGS_MASTER + str(master_id), headers=HEADERS)
+    result = {'styles': r.json().get('styles'),
+              'genres': r.json().get('genres'),
+              'title': r.json().get('title'),
+              'artists': r.json().get('artists'),
+              'year': r.json().get('year'),
+              'images': r.json().get('images'),
+              'tracklist': r.json().get('tracklist')}
+    return render_template('album.html', result=result)
--- config.py
+++ config.py
@@ -7,4 +7,8 @@ SECRET_KEY = os.environ.get('FLASK_SECRET')
 # Discogs
 TOKEN = os.environ.get('DISCOGS_TOKEN')
 HEADERS = {'Authorization': 'Discogs token=' + TOKEN, 'User-Agent': 'RhymeApp/0.1'}
-DISCOGS_API = 'https://api.discogs.com/database/search'
+DISCOGS_QUERY = 'https://api.discogs.com/database/search'
+DISCOGS_MASTER = 'https://api.discogs.com/masters/'
+
+# VK
+APP_ID = os.environ.get('VK_APP_ID')
