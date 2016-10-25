c446d676c1191d8d6475ee347cefe189a5fea30d
Kazauwa <i.heart.d.dead@gmail.com>
2016-10-25 19:36:54 +0300

    Added more fields, removed saving to JSON

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, Optional
 
 
 class SearchForm(FlaskForm):
-    search_by_album = StringField('search_by_album', validators=[DataRequired()])
-    save_to_json = BooleanField('save_to_json', default=False)
+    search_by_album = StringField('search_by_album', validators=[Optional()])
+    search_by_artist = StringField('search_by_artist', validators=[Optional()])
+    search_track = StringField('search_track', validators=[Optional()])
+    year = IntegerField('year', validators=[Optional()])
+
+    def validate(self):
+        if not super().validate():
+            return False
+        if not self.search_by_album and not self.search_by_artist and not self.search_track:
+            msg = 'At least one of the search fields must be set!'
+            self.search_by_album.errors.append(msg)
+            self.search_by_artist.errors.append(msg)
+            self.search_track.errors.append(msg)
+            return False
+        return True
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 import requests
-import json
-from flask import render_template, redirect, url_for, session
+from flask import render_template, redirect, url_for
 from app import app
 from .forms import SearchForm
 from config import HEADERS, DISCOGS_QUERY, DISCOGS_MASTER
@@ -17,7 +16,6 @@ def index():
 def search():
     form = SearchForm()
     if form.validate_on_submit():
-        session['save_data'] = form.save_to_json.data
         return redirect(url_for('search_results', query=form.search_by_album.data))
     return render_template('search.html', form=form)
 
@@ -30,14 +28,9 @@ def search_results(query):
                           'genre': result.get('genre'),
                           'thumb': result.get('thumb'),
                           'year': result.get('year')} for count, result in enumerate(r.json()['results'])}
-    if session['save_data']:
-        with open('data.json', 'a') as writer:
-            for key, value in result.items():
-                json.dump({key: value}, writer)
-                writer.write('\n')
     return render_template('search_results.html',
-                            query=query,
-                            results=result.values())
+                           query=query,
+                           results=result.values())
 
 
 @app.route('/album/id<master_id>')
